ae20e0b862db63b513f1ebd68b62113344a92e20
bill9800 <bill980000@gmail.com>
2018-10-23 20:44:18 -0700

    upload audio model

---
+++ model/lib/model_AO.py
@@ -0,0 +1,149 @@
+from keras import optimizers
+#from keras.layers import Dense, Convolution3D, MaxPooling3D, ZeroPadding3D, Dropout, Flatten, BatchNormalization, ReLU
+from keras.models import Sequential, model_from_json
+from keras import optimizers
+from keras.layers import Input, Dense, Convolution2D, Deconvolution2D, Bidirectional
+from keras.layers import Dropout, Flatten, BatchNormalization, ReLU, Reshape, Permute
+from keras.layers.merge import concatenate
+from keras.models import Model, load_model
+from keras.layers.recurrent import LSTM
+from keras.initializers import he_normal
+import numpy as np
+from keras.callbacks import ModelCheckpoint
+from keras.callbacks import TensorBoard
+import tensorflow as tf
+import os
+
+
+def AO_model(people_num=2):
+    model_input = Input(shape=(298, 257, 2))
+    print('0:', model_input.shape)
+    conv1 = Convolution2D(96, kernel_size=(1, 7), strides=(1, 1), padding='same', dilation_rate=(1, 1), name='conv1')(model_input)
+    conv1 = BatchNormalization()(conv1)
+    conv1 = ReLU()(conv1)
+    print('1:', conv1.shape)
+
+    conv2 = Convolution2D(96, kernel_size=(7, 1), strides=(1, 1), padding='same', dilation_rate=(1, 1), name='conv2')(conv1)
+    conv2 = BatchNormalization()(conv2)
+    conv2 = ReLU()(conv2)
+    print('2:', conv2.shape)
+
+    conv3 = Convolution2D(96, kernel_size=(5, 5), strides=(1, 1), padding='same', dilation_rate=(1, 1), name='conv3')(conv2)
+    conv3 = BatchNormalization()(conv3)
+    conv3 = ReLU()(conv3)
+    print('3:', conv3.shape)
+
+    conv4 = Convolution2D(96, kernel_size=(5, 5), strides=(1, 1), padding='same', dilation_rate=(2, 1), name='conv4')(conv3)
+    conv4 = BatchNormalization()(conv4)
+    conv4 = ReLU()(conv4)
+    print('4:', conv4.shape)
+
+    conv5 = Convolution2D(96, kernel_size=(5, 5), strides=(1, 1), padding='same', dilation_rate=(4, 1), name='conv5')(conv4)
+    conv5 = BatchNormalization()(conv5)
+    conv5 = ReLU()(conv5)
+    print('5:', conv5.shape)
+
+    conv6 = Convolution2D(96, kernel_size=(5, 5), strides=(1, 1), padding='same', dilation_rate=(8, 1), name='conv6')(conv5)
+    conv6 = BatchNormalization()(conv6)
+    conv6 = ReLU()(conv6)
+    print('6:', conv6.shape)
+
+    conv7 = Convolution2D(96, kernel_size=(5, 5), strides=(1, 1), padding='same', dilation_rate=(16, 1), name='conv7')(conv6)
+    conv7 = BatchNormalization()(conv7)
+    conv7 = ReLU()(conv7)
+    print('7:', conv7.shape)
+
+    conv8 = Convolution2D(96, kernel_size=(5, 5), strides=(1, 1), padding='same', dilation_rate=(32, 1), name='conv8')(conv7)
+    conv8 = BatchNormalization()(conv8)
+    conv8 = ReLU()(conv8)
+    print('8:', conv8.shape)
+
+    conv9 = Convolution2D(96, kernel_size=(5, 5), strides=(1, 1), padding='same', dilation_rate=(1, 1), name='conv9')(conv8)
+    conv9 = BatchNormalization()(conv9)
+    conv9 = ReLU()(conv9)
+    print('9:', conv9.shape)
+
+    conv10 = Convolution2D(96, kernel_size=(5, 5), strides=(1, 1), padding='same', dilation_rate=(2, 2), name='conv10')(conv9)
+    conv10 = BatchNormalization()(conv10)
+    conv10 = ReLU()(conv10)
+    print('10:', conv10.shape)
+
+    conv11 = Convolution2D(96, kernel_size=(5, 5), strides=(1, 1), padding='same', dilation_rate=(4, 4), name='conv11')(conv10)
+    conv11 = BatchNormalization()(conv11)
+    conv11 = ReLU()(conv11)
+    print('11:', conv11.shape)
+
+    conv12 = Convolution2D(96, kernel_size=(5, 5), strides=(1, 1), padding='same', dilation_rate=(8, 8), name='conv12')(conv11)
+    conv12 = BatchNormalization()(conv12)
+    conv12 = ReLU()(conv12)
+    print('12:', conv12.shape)
+
+    conv13 = Convolution2D(96, kernel_size=(5, 5), strides=(1, 1), padding='same', dilation_rate=(16, 16), name='conv13')(conv12)
+    conv13 = BatchNormalization()(conv13)
+    conv13 = ReLU()(conv13)
+    print('13:', conv13.shape)
+
+    conv14 = Convolution2D(96, kernel_size=(5, 5), strides=(1, 1), padding='same', dilation_rate=(32, 32), name='conv14')(conv13)
+    conv14 = BatchNormalization()(conv14)
+    conv14 = ReLU()(conv14)
+    print('14:', conv14.shape)
+
+    conv15 = Convolution2D(8, kernel_size=(1, 1), strides=(1, 1), padding='same', dilation_rate=(1, 1), name='conv15')(conv14)
+    conv15 = BatchNormalization()(conv15)
+    conv15 = ReLU()(conv15)
+    print('15:', conv15.shape)
+
+    AVfusion = Reshape((298, 8 * 257))(conv15)
+    print('AVfusion:', AVfusion.shape)
+
+    lstm = Bidirectional(LSTM(200, return_sequences=True))(AVfusion)
+    print('lstm:', lstm.shape)
+
+    fc1 = Dense(600, name="fc1", activation='relu', kernel_initializer=he_normal(seed=27))(lstm)
+    print('fc1:', fc1.shape)
+    fc2 = Dense(600, name="fc2", activation='relu', kernel_initializer=he_normal(seed=42))(fc1)
+    print('fc2:', fc2.shape)
+    fc3 = Dense(600, name="fc3", activation='relu', kernel_initializer=he_normal(seed=65))(fc2)
+    print('fc3:', fc3.shape)
+
+    complex_mask = Dense(257 * 2 * people_num, name="complex_mask", activation='sigmoid', kernel_initializer=he_normal(seed=65))(fc3)
+    print('complex_mask:', complex_mask.shape)
+
+    complex_mask_out = Reshape((298, 257, 2, people_num))(complex_mask)
+    print('complex_mask_out:', complex_mask_out.shape)
+
+    # --------------------------- AO end ---------------------------
+    AO_model = Model(inputs=model_input, outputs=complex_mask_out)
+
+    # compile AO_model
+    AO_model.compile(optimizer='adam', loss='mse')
+
+    return AO_model
+
+
+if __name__ == '__main__':
+    people_num = 2
+    audio_input = np.random.rand(5, 298, 257, 2)        # 5 audio parts, (298, 257, 2) stft feature
+    audio_label = np.random.rand(5, 298, 257, 2, people_num)     # 5 audio parts, (298, 257, 2) stft feature, people num to be defined
+
+    path = './saved_models_AO'
+    folder = os.path.exists(path)
+
+    if not folder:
+        os.makedirs(path)
+        print('create folder to save models')
+
+    filepath = path + "/AOmodel-" + str(people_num) + "p-{epoch:02d}-{val_loss:.4f}.h5"
+    checkpoint = ModelCheckpoint(filepath, monitor='val_loss', verbose=1, save_best_only=True, mode='min')
+
+    AO_model = AO_model(people_num)
+    AO_model.fit(audio_input, audio_label,
+                 epochs=5,
+                 batch_size=2,
+                 validation_data=(audio_input, audio_label),
+                 shuffle=False,
+                 callbacks=[TensorBoard(log_dir='./log_AO'), checkpoint])
+
+    # AO_model = load_model('./saved_models_AO/***.h5')     # this line shows how to load model
+    # AO_model.predict(audio_input)
+
