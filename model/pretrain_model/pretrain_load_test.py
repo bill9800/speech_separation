3ae5c253b7b6b3eacb055ac5f230b4de81d00fd6
bill9800 <bill980000@gmail.com>
2018-11-24 16:20:11 -0800

    add model test

--- model/pretrain_model/pretrain_load_test.py
+++ model/pretrain_model/pretrain_load_test.py
@@ -4,33 +4,40 @@ sys.path.append('../lib')
 import utils
 from tensorflow.python.framework import tensor_util
 import numpy as np
+from keras.models import load_model
+from keras.models import Model
+## paraeter
+PB = 0
+CKPT = 0
+HDF5 = 1
 
 data = np.random.randint(256,size=(1,160,160,3),dtype='int32')
 
 
-##############
+###############
 graph_path = 'FaceNet_new/20180402-114759.pb'
 
-graph = utils.load_graph(graph_path,tensorboard=False)
 # utils.inspect_operation(graph_path,'ops.txt')
+if PB:
+    with tf.gfile.FastGFile(graph_path,'rb') as f:
+        graph_def = tf.GraphDef()
+        graph_def.ParseFromString(f.read())
+        with tf.Session() as sess:
+            #sess.graph.as_default()
+            tf.import_graph_def(graph_def)
+            print(sess.run('import/embeddings', feed_dict={'import/batch_size:0': data,'import/phase_train:0':False}))
 
-x = graph.get_tensor_by_name('batch_size:0')
-y = graph.get_tensor_by_name('phase_train:0')
-z = graph.get_tensor_by_name('embeddings:0')
+if CKPT:
+    saver = tf.train.import_meta_graph('FaceNet_new/model-20180402-114759.meta')
+    with tf.Session() as sess:
+        saver.restore(sess=sess,save_path='FaceNet_new/model-20180402-114759.ckpt-275')
+        #print(sess.run('embeddings:0', feed_dict={'batch_size:0': data, 'phase_train:0': False}))
 
-with tf.Session(graph=graph) as sess:
-    print(sess.run(z,feed_dict={x:data,y:0}))
-
-'''
-with tf.Session() as sess:
-    saver = tf.train.import_meta_graph('FaceNet_old/model-20170512-110547.meta')
-    saver.restore(sess,'FaceNet_old/model-20170512-110547.ckpt-250000.data-00000-of-00001')
-    print(sess.run(global_step_tensor))
-'''
-
-
-
-
+if HDF5:
+    model = load_model('FaceNet_keras/facenet_keras.h5')
+    model.summary()
+    avgPool_layer_model = Model(inputs=model.input,outputs=model.get_layer('AvgPool').output)
+    print(avgPool_layer_model.predict(data))
 
 
 
