b882e8603b581702d0cba282c030cfa46066a337
bill9800 <bill980000@gmail.com>
2018-11-27 16:32:30 -0800

    model with both audio and visual as input

---
+++ model/model_v2/AV_model_eval.py
@@ -0,0 +1,98 @@
+## evaluate the model and generate the prediction
+import sys
+sys.path.append('../lib')
+from keras.models import load_model
+from model_ops import ModelMGPU
+import os
+import scipy.io.wavfile as wavfile
+import numpy as np
+import utils
+import tensorflow as tf
+# super parameters
+people_num = 2
+NUM_GPU = 1
+
+# PATH
+model_path = './saved_models_AO_with_norm/AOmodel-2p-015-0.02258.h5'
+dir_path = './pred/'
+if not os.path.isdir(dir_path):
+    os.mkdir(dir_path)
+database_path = '../../data/audio/audio_database/mix/'
+face_path = '../../data/video/face_emb/'
+
+# load data
+testfiles = []
+with open('../../data/AV_log/AVdataset_train.txt', 'r') as f:
+    testfiles = f.readlines()
+
+def parse_X_data(line,num_people=people_num,database_path=database_path,face_path=face_path):
+    parts = line.split() # get each name of file for one testset
+    mix_str = parts[0]
+    name_list = mix_str.replace('.npy','')
+    name_list = name_list.replace('mix-','',1)
+    names = name_list.split('-')
+    single_idxs = []
+    for i in range(num_people):
+        single_idxs.append(names[i])
+    file_path = database_path + mix_str
+    mix = np.load(file_path)
+    face_embs = np.zeros((1,75,1,1792,num_people))
+    for i in range(num_people):
+        face_embs[1,:,:,:,i] = np.load(face_path+"%05d_face_emb.npy"%single_idxs[i])
+
+    return mix,single_idxs,face_embs
+
+
+# predict data
+AV_model = load_model(model_path,custom_objects={"tf": tf})
+if NUM_GPU > 1:
+    parallel_model = ModelMGPU(AV_model,NUM_GPU)
+    for line in testfiles:
+        mix,single_idxs,face_embs = parse_X_data(line)
+        mix_expand = np.expand_dims(mix, axis=0)
+        cRMs = parallel_model.predict([mix_expand,face_embs])
+        cRMs = cRMs[0]
+        prefix = ""
+        for idx in single_idxs:
+            prefix += idx + "-"
+        for i in range(len(cRMs)):
+            cRM = cRMs[:,:,:,i]
+            assert cRM.shape == (298,257,2)
+            F = utils.fast_icRM(mix,cRM)
+            T = utils.fast_istft(F,power=False)
+            filename = dir_path+prefix+single_idxs[i]+'.wav'
+            wavfile.write(filename,16000,T)
+
+
+if NUM_GPU <= 1:
+    for line in testfiles:
+        mix, single_idxs, face_embs = parse_X_data(line)
+        mix_expand = np.expand_dims(mix, axis=0)
+        cRMs = AV_model.predict([mix_expand, face_embs])
+        cRMs = cRMs[0]
+        prefix = ""
+        for idx in single_idxs:
+            prefix += idx + "-"
+        for i in range(people_num):
+            cRM = cRMs[:,:,:,i]
+            assert cRM.shape == (298,257,2)
+            F = utils.fast_icRM(mix,cRM)
+            T = utils.fast_istft(F,power=False)
+            filename = dir_path+prefix+single_idxs[i]+'.wav'
+            wavfile.write(filename,16000,T)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ model/model_v2/AV_train.py
@@ -0,0 +1,134 @@
+import sys
+sys.path.append('../lib')
+import model_AV_new as AV
+from model_ops import ModelMGPU,latest_file
+from keras.callbacks import ModelCheckpoint, LearningRateScheduler, Callback
+from keras.models import Model, load_model
+from MyGenerator import AVGenerator
+from keras.callbacks import TensorBoard
+from keras import optimizers
+import os
+from model_loss import audio_discriminate_loss2 as audio_loss
+import tensorflow as tf
+
+# create AV model
+#############################################################
+RESTORE = False
+# If set true, continue training from last checkpoint
+# needed change 1:h5 file name, 2:epochs num, 3:initial_epoch
+
+# super parameters
+people_num = 2
+epochs = 100
+initial_epoch = 0
+batch_size = 2 # 4 to feed one 16G GPU
+gamma_loss = 0.1
+beta_loss = gamma_loss*2
+
+# physical devices option to accelerate training process
+workers = 1 # num of core
+use_multiprocessing = False
+NUM_GPU = 1
+
+# PATH
+path = './saved_AV_models' # model path
+database_dir_path = '../../data/'
+#############################################################
+
+# create folder to save models
+folder = os.path.exists(path)
+if not folder:
+    os.makedirs(path)
+    print('create folder to save models')
+filepath = path + "/AVmodel-" + str(people_num) + "p-{epoch:03d}-{val_loss:.5f}.h5"
+checkpoint = ModelCheckpoint(filepath, monitor='val_loss', verbose=1, save_best_only=True, mode='min')
+
+
+#############################################################
+# automatically change lr
+def scheduler(epoch):
+    ini_lr = 0.00001
+    lr = ini_lr
+    if epoch >= 5:
+        lr = ini_lr / 5
+    if epoch >= 10:
+        lr = ini_lr / 10
+    return lr
+
+rlr = LearningRateScheduler(scheduler, verbose=1)
+#############################################################
+# read train and val file name
+# format: mix.npy single.npy single.npy
+trainfile = []
+valfile = []
+with open((database_dir_path+'AV_log/AVdataset_train.txt'), 'r') as t:
+    trainfile = t.readlines()
+with open((database_dir_path+'AV_log/AVdataset_val.txt'), 'r') as v:
+    valfile = v.readlines()
+# ///////////////////////////////////////////////////////// #
+
+# the training steps
+if RESTORE:
+    latest_file = latest_file(path+'/')
+    AV_model = load_model(latest_file,custom_objects={"tf": tf})
+    info = latest_file.strip().split('-')
+    initial_epoch = int(info[-2])
+else:
+    AV_model = AV.AV_model(people_num)
+
+train_generator = AVGenerator(trainfile,database_dir_path= database_dir_path, batch_size=batch_size, shuffle=True)
+val_generator = AVGenerator(valfile,database_dir_path=database_dir_path, batch_size=batch_size, shuffle=True)
+
+if NUM_GPU > 1:
+    parallel_model = ModelMGPU(AV_model,NUM_GPU)
+    adam = optimizers.Adam()
+    loss = audio_loss(gamma=gamma_loss,beta=beta_loss,num_speaker=people_num)
+    parallel_model.compile(loss=loss,optimizer=adam)
+    print(AV_model.summary())
+    parallel_model.fit_generator(generator=train_generator,
+                           validation_data=val_generator,
+                           epochs=epochs,
+                           workers = workers,
+                           use_multiprocessing= use_multiprocessing,
+                           callbacks=[TensorBoard(log_dir='./log_AV'), checkpoint, rlr],
+                           initial_epoch=initial_epoch
+                           )
+if NUM_GPU <= 1:
+    adam = optimizers.Adam()
+    loss = audio_loss(gamma=gamma_loss,beta=beta_loss, num_speaker=people_num)
+    AV_model.compile(optimizer=adam, loss=loss)
+    print(AV_model.summary())
+    AV_model.fit_generator(generator=train_generator,
+                           validation_data=val_generator,
+                           epochs=epochs,
+                           workers = workers,
+                           use_multiprocessing= use_multiprocessing,
+                           callbacks=[TensorBoard(log_dir='./log_AV'), checkpoint, rlr],
+                           initial_epoch=initial_epoch
+                           )
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
