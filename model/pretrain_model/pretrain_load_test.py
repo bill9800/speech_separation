ac579259476db4d0b805795a7b7276af1920794d
bill9800 <bill980000@gmail.com>
2018-12-01 11:48:58 -0800

    real data test

--- model/pretrain_model/pretrain_load_test.py
+++ model/pretrain_model/pretrain_load_test.py
@@ -1,11 +1,15 @@
 import tensorflow as tf
 import sys
-sys.path.append('../lib')
+sys.path.append('../../model/lib/')
 import utils
 from tensorflow.python.framework import tensor_util
 import numpy as np
 from keras.models import load_model
 from keras.models import Model
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+import os
+
 ## paraeter
 PB = 0
 CKPT = 0
@@ -34,10 +38,48 @@ if CKPT:
         #print(sess.run('embeddings:0', feed_dict={'batch_size:0': data, 'phase_train:0': False}))
 
 if HDF5:
+    save_path = './face1022_emb/'
+    if not os.path.exists(save_path):
+        os.makedirs(save_path)
+
     model = load_model('FaceNet_keras/facenet_keras.h5')
     model.summary()
     avgPool_layer_model = Model(inputs=model.input,outputs=model.get_layer('AvgPool').output)
-    print(avgPool_layer_model.predict(data))
+    # print(avgPool_layer_model.predict(data))
+
+    lines = []
+    with open('./valid_frame.txt', 'r') as f:
+        lines = f.readlines()
+
+    for line in lines:
+        embtmp = np.zeros((75, 1, 1792))
+        headname = line.strip()
+        tailname = ''
+        for i in range(1, 76):
+            if i < 10:
+                tailname = '_0{}.jpg'.format(i)
+            else:
+                tailname = '_' + str(i) + '.jpg'
+            picname = headname + tailname
+            # print(picname)
+            I = mpimg.imread('./face_input/' + picname)
+            I_np = np.array(I)
+            I_np = I_np[np.newaxis, :, :, :]
+            # print(I_np.shape)
+            # print(avgPool_layer_model.predict(I_np).shape)
+            embtmp[i - 1, :] = avgPool_layer_model.predict(I_np)
+
+        # print(embtmp.shape)
+        people_index = int(line.strip().split('_')[1])
+        npname = '{:05d}_face_emb.npy'.format(people_index)
+        print(npname)
+
+        np.save(save_path + npname, embtmp)
+        with open('faceemb_dataset.txt', 'a') as d:
+            d.write(npname + '\n')
+
+
+
 
 
 
